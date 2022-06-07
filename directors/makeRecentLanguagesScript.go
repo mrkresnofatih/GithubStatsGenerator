3f1cfec25e71b91b5205719fc4af78f8fb8b0d27
mrkresnofatih <mrkresnofatih@gmail.com>
2022-06-08 06:05:19 +0700

    make myRecentLanguages randomize colors

--- directors/makeRecentLanguagesScript.go
+++ directors/makeRecentLanguagesScript.go
@@ -19,7 +19,10 @@ func MakeRecentLanguagesScript(data []string) {
 	utils.HandleIfError(err2)
 	log.Println("Created copy template CMD")
 
+	colorPallette := utils.GetColorPallete()
+
 	for index, value := range data {
+		color := colorPallette[index]
 		cmdSed := "sed -i 's/@@@AVAILABLE" + strconv.Itoa(index) + "//' myRecentLanguages.svg \n"
 		_, errr := f.WriteString(cmdSed)
 		utils.HandleIfError(errr)
@@ -30,6 +33,10 @@ func MakeRecentLanguagesScript(data []string) {
 		_, errrr := f.WriteString(cmdSd)
 		utils.HandleIfError(errrr)
 
+		cmdsd := "sed -i 's/@@@COLOR" + strconv.Itoa(index+1) + "/" + color + "/' myRecentLanguages.svg \n"
+		_, e := f.WriteString(cmdsd)
+		utils.HandleIfError(e)
+
 		log.Println("Created Replace CMD for @@@LANG" + strconv.Itoa(index))
 	}
 
@@ -37,6 +44,11 @@ func MakeRecentLanguagesScript(data []string) {
 		cmd := "sed -i 's/@@@AVAILABLE" + strconv.Itoa(i) + "/opacity:0;/' myRecentRepositories.svg \n"
 		_, er := f.WriteString(cmd)
 		utils.HandleIfError(er)
+
+		cmds := "sed -i 's/@@@COLOR" + strconv.Itoa(i+1) + "/transparent/' myRecentRepositories.svg \n"
+		_, es := f.WriteString(cmds)
+		utils.HandleIfError(es)
+
 		log.Println("Created Cleanup Template CMD for @@@AVAILABLE" + strconv.Itoa(i))
 	}
 }
